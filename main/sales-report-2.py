d04bbe1e120d948877cd12f076b0838c20c80b77
cowardclaire <cowardclaire@hotmail.co.uk>
2025-11-12 18:13:35 +0000

    adding 2 dimensional table to sales report

--- main/sales-report-2.py
+++ main/sales-report-2.py
@@ -9,3 +9,7 @@ sales_data_1d = np.random.randint(low =75, high = 200, size = 12) #1d  reference
 
 print(f'Here is our sales data in 1D: \n{sales_data_1d}')
 
+sales_data_2d = sales_data_1d.reshape(4, 3) #reshaping the 1d array into a 2d array with 4 rows and 3 columns
+
+print(f'\nHere is our sales data in 2D: \n{sales_data_2d}')
+
