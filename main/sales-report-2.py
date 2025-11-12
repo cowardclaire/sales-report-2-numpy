71e59a962c4f91f57f4cf6aa6cb93a81c5ce6072
cowardclaire <cowardclaire@hotmail.co.uk>
2025-11-12 18:06:40 +0000

    Update sales report logic

--- main/sales-report-2.py
+++ main/sales-report-2.py
@@ -5,6 +5,7 @@
 import numpy as np
 
 np.random.seed(seed=42)
-sales_data_4dweek = np.random.randint(low =75, high = 200, size = 12) #4dweek references its a a 4 day week sales report
+sales_data_1d = np.random.randint(low =75, high = 200, size = 12) #1d  references its a a 1 dimensional array
 
-print(f'Here are our sales over the 4 days of this week: \n{sales_data_4dweek}')
+print(f'Here is our sales data in 1D: \n{sales_data_1d}')
+
