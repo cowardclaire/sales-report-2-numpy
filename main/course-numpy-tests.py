f2cf74faf89794d9af6c9e6e2fb43705f87f2692
cowardclaire <cowardclaire@hotmail.co.uk>
2025-11-13 18:15:16 +0000

    "final update thurs 13.11"

--- main/course-numpy-tests.py
+++ main/course-numpy-tests.py
@@ -1,6 +1,8 @@
 
 import numpy as np
 
-my_2dlist = [['A', 'B'],['C', 'D']]
-my_2darray = np.array(my_2dlist)
-print(f'Here is my 2D list:\n{my_2dlist}')
+np.random.seed(seed=101)
+arr = np.random.randint(low=10,high=50,size=(3,5))
+print(arr)
+print(arr[:,1])
+
