1b7ba53561bd4a699a0ce6b31fda4e179d49c9d8
cowardclaire <cowardclaire@hotmail.co.uk>
2025-11-16 08:44:16 +0000

    updating with imaging workings

--- main/course-numpy-tests.py
+++ main/course-numpy-tests.py
@@ -1,23 +1,14 @@
 
 import numpy as np
 
-macro_arr=np.array([[513850, 0.4, 0.5],[516876, 0.3, 0.5],[518915, 0.4, 0.5],[521920, 0.4, 0.5],[523813, 0.7, 0.5],[526750, 0.7, 0.5],[528712, 1.0, 0.25],[532082, 1.5, 0.25]])
-print(macro_arr[:,0])
+import matplotlib.pyplot as plt
+import PIL
+import urllib.request
 
-gdp_arr = macro_arr[0:4,0]
-sum_gdp_arr = np.sum(gdp_arr)
-print("Sum of GDP array:", sum_gdp_arr)
-
-cpi_arr = macro_arr[:,1]
-bank_rate_arr = macro_arr[:,2]  
-
-interest_rate_arr = bank_rate_arr - cpi_arr
-print("Interest Rate Array:", interest_rate_arr)
-
-mean_2nd_3rd_col_arr = macro_arr[:,1:3]
-mean_values = np.mean(mean_2nd_3rd_col_arr, axis=0)
-print(mean_values)
-
-std_2nd_3rd_col_arr = macro_arr[:,1:3]
-std_values = np.std(std_2nd_3rd_col_arr, axis=0)
-print(std_values)
+image_url = "https://ci-jshint.herokuapp.com/static/images/logo.png"
+image_data = urllib.request.urlopen(image_url)
+image = PIL.Image.open(image_data)
+np_image = np.array(image)
+type(np_image)
+plt.imshow(np_image*2)
+plt.show()
